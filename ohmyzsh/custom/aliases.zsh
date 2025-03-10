c7053f8884ebbafa29a4a5cb43946741b677c920
Janne Suominen <janne.suominen@siili.com>
2025-03-10 07:45:31 +0200

    Add new mac stuff

---
+++ ohmyzsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+
+alias ll='ls -la'
+alias l='ls -l'
+alias la='ls -a'
+alias lh='ls -lh'
+alias lha='ls -lha'
+alias lha='ls -lha'
+
+alias ddrush='ddev drush'
+alias dcomp='ddev composer'
+alias dnpm='ddev npm'
+alias dyarn='ddev yarn'
+alias dpnpm='ddev pnpm'
+
